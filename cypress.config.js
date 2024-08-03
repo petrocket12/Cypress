f514111a38522f449b1d993bc39cd3e6bc71ae18
petrocket12 <megapascal5261@gmail.com>
2024-08-03 13:01:13 +0200

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    //to add base url
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
